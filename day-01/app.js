a53136a47ed2766381f3cdff80089051ff2d21ba
vishwas28-cpu <vishwas.work28@gmail.com>
2026-01-29 21:46:19 +0530

    day1 commit 5

--- day-01/app.js
+++ day-01/app.js
@@ -1,8 +1,4 @@
 console.log("day 1 backenedd");
-console.log("day 1 backenedd");
-console.log("day 1 backenedd");
-console.log("day 1 backenedd");
-console.log("day 1 backenedd");
-console.log("day 1 backenedd");
+
 
 
