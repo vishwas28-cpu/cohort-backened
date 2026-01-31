e3becb0aa745e4a89d8a465b1da2213b16f427b3
vishwas28-cpu <vishwas.work28@gmail.com>
2026-01-31 22:09:40 +0530

    commit 2

--- day-01/app.js
+++ day-01/app.js
@@ -10,5 +10,3 @@ console.log("hello world");console.log("hello world");console.log("hello world")
 console.log("hello world");
 console.log("hello world");
 console.log("hello world");
-console.log("hello world");
-console.log("hello world");
