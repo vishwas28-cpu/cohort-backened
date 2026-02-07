8f1065516bac1cd53dcfa01c17ca0f7c13dba4b8
vishwas28-cpu <vishwas.work28@gmail.com>
2026-02-07 23:54:39 +0530

    update 5

--- day-01/app.js
+++ day-01/app.js
@@ -1,29 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000);
-const express = require("express");
-const app = express();
-
-app.listen(3000);
-const express = require("express");
-const app = express();
-
-app.listen(3000);
-const express = require("express");
-const app = express();
-
-app.listen(3000);
-const express = require("express");
-const app = express();
-
-app.listen(3000);
-const express = require("express");
-const app = express();
-
-app.listen(3000);
-const express = require("express");
-const app = express();
 
 
 
