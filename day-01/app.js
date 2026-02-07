b6029723c281050b29ea96e4c44bdeb69ae57c8d
vishwas28-cpu <vishwas.work28@gmail.com>
2026-02-07 23:53:10 +0530

    update

--- day-01/app.js
+++ day-01/app.js
@@ -41,8 +41,3 @@ app.listen(3000);
 const express = require("express");
 const app = express();
 
-app.listen(3000);
-const express = require("express");
-const app = express();
-
-app.listen(3000);
