00f2a9d4ac173682531ef29d9cb2e61aece1bd07
vishwas28-cpu <vishwas.work28@gmail.com>
2026-02-11 21:41:05 +0530

    add server 1

--- day-02/app.js
+++ day-02/app.js
@@ -2,3 +2,74 @@ const express = require('express');
 const app = express();
 
 app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)const express = require('express');
+const app = express();
+
+app.listen(3000)
