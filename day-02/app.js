f1f4926b57e4c50fada3842c481a96a2555c0355
vishwas28-cpu <vishwas.work28@gmail.com>
2026-02-15 21:25:22 +0530

    add server 21

--- day-02/app.js
+++ day-02/app.js
@@ -3,3 +3,43 @@ const app = express();
 
 app.listen(3000)
 
+const express = require('express');
+const app = express();
+
+app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(3000)
+
